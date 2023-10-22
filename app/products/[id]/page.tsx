fcaff4ff757927c56e03aca538ae20fa71a44f96
Suliman Badour <71437804+sulimanbadour1@users.noreply.github.com>
2023-10-23 00:38:37 +0200

    updated ui

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -1,4 +1,7 @@
 import { getProductByID } from "@/lib/actions";
+import { Product } from "@/types";
+import Image from "next/image";
+import Link from "next/link";
 import { redirect } from "next/navigation";
 
 import { type } from "os";
@@ -7,9 +10,72 @@ type props = {
   params: { id: string };
 };
 const ProductDetails = async ({ params: { id } }: props) => {
-  const product = await getProductByID(id);
+  const product: Product = await getProductByID(id);
   if (!product) redirect("/");
-  return <div>{product.title}</div>;
+  return (
+    <div className="product-container">
+      <div className="flex gap-28 xl:flex-row flex-col">
+        <div className="product-image">
+          <Image
+            src={product.image}
+            alt={product.title}
+            width={580}
+            height={400}
+            className="mx-auto"
+          />
+        </div>
+        <div className="flex-1 flex flex-col">
+          <div
+            className="flex justify-between items-start 
+          gap-5 flex-wrap pb-5"
+          >
+            <div className="flex flex-col gap-3">
+              <p className="text-[28px] text-secondary font-semibold">
+                {product.title}
+              </p>
+              <Link
+                href={product.url}
+                target="_blank"
+                className="text-base text-black opacity-50"
+              >
+                Visit Product
+              </Link>
+            </div>
+            <div className="flex items-center gap-3">
+              <div className="product-hearts">
+                <Image
+                  src="/assets/icons/red-heart.svg"
+                  alt="heart"
+                  width={20}
+                  height={20}
+                />
+                <p className="text-base font-semibold text-[#D46F77]">
+                  {product.reviewsCount}
+                </p>
+              </div>
+              <div className="p-2 bg-white-200 rounded-10  ">
+                <Image
+                  src="/assets/icons/bookmark.svg"
+                  alt="share"
+                  width={20}
+                  height={20}
+                />
+              </div>
+              <div className="p-2 bg-white-200 rounded-10  ">
+                <Image
+                  src="/assets/icons/share.svg"
+                  alt="share"
+                  width={20}
+                  height={20}
+                />
+              </div>
+            </div>
+          </div>
+          <div className="product-info"></div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ProductDetails;

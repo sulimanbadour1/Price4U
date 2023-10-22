b8d6d6ad373003af17107fdfaa77a1c4e0b42b4b
Suliman Badour <71437804+sulimanbadour1@users.noreply.github.com>
2023-10-23 00:12:52 +0200

    added product details page

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -1,7 +1,15 @@
-import React from "react";
+import { getProductByID } from "@/lib/actions";
+import { redirect } from "next/navigation";
 
-const ProductDetails = () => {
-  return <div>ProductDetails</div>;
+import { type } from "os";
+import React from "react";
+type props = {
+  params: { id: string };
+};
+const ProductDetails = async ({ params: { id } }: props) => {
+  const product = await getProductByID(id);
+  if (!product) redirect("/");
+  return <div>{product.title}</div>;
 };
 
 export default ProductDetails;
--- components/ProductCard.tsx
+++ components/ProductCard.tsx
@@ -1,10 +1,36 @@
 import { Product } from "@/types";
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 interface props {
   product: Product;
 }
 const ProductCard = ({ product }: props) => {
-  return <div>ProductCard</div>;
+  return (
+    <Link className="product-card" href={`/products/${product._id}`}>
+      <div className="product-card_img-container">
+        <Image
+          src={product.image}
+          width={200}
+          height={200}
+          alt={product.title}
+          className="product-card-image"
+        />
+      </div>
+      <div className="flex flex-col gap-3">
+        <h3 className="product-title">{product.title}</h3>
+        <div className="flex justify-between">
+          <p className="text-black opacity-50 text-lg capitalize">
+            {product.category}
+          </p>
+          <p className="text-black text-lg font-semibold">
+            <span>{product?.currency}</span>
+            <span>{product.currentPrice}</span>
+          </p>
+        </div>
+      </div>
+    </Link>
+  );
 };
 
 export default ProductCard;
